45fbdc6da3c8dc3bea9df355767ba4da7ed5d51e
mloewent <mkavone@gmail.com>
2015-01-24 20:53:38 -0800

    added level 3

--- GravityScramble/Assets/Scripts/SmoothCamera2d.cs
+++ GravityScramble/Assets/Scripts/SmoothCamera2d.cs
@@ -9,6 +9,17 @@ namespace Assets.Scripts
       private Vector3 velocity = Vector3.zero;
       public Transform target;
 
+      void Start()
+      {
+         if (target)
+         {
+            Vector3 point = camera.WorldToViewportPoint(target.position);
+            Vector3 delta = target.position - camera.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, point.z)); //(new Vector3(0.5, 0.5, point.z));
+            Vector3 destination = transform.position + delta;
+            transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);
+         }
+      }
+
       // Update is called once per frame
       void Update()
       {
